0c0da3e7d851c5ce464180b8070b271bd5943dd6
Roybal_A <Roybal.usmc@gmail.com>
2019-02-01 19:07:52 -0800

    added pathfinding using unity built in navmesh, added player movement with enemy detections, added proper movement and attack animations, set up enemy character with animations and statemachines

---
+++ Assets/Scripts/CameraControls.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControls : MonoBehaviour
+{
+    //------------------------------------------------------
+    //used for camera movement
+    //------------------------------------------------------
+    private float speedH = 1.0f;
+    private float speedV = 1.0f;
+    private float yaw;
+    private float pitch;
+    private float axisX;
+    private float axisZ;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        ControlCamera();
+
+    }
+
+
+    //enables camera movement
+    private void ControlCamera()
+    {
+        //mouse look
+        yaw += speedH * Input.GetAxis("Mouse X");
+        pitch -= speedV * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+
+        //keyboard movement
+        axisX = Input.GetAxis("Horizontal");
+        axisZ = Input.GetAxis("Vertical");
+
+        transform.Translate(axisX, 0.0f, axisZ);
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float shootRange;
-    [SerializeField] private float shootSpeed;
-    private float rechargeTime = 2f;
+    [SerializeField] private Camera mainCamera;  //reference to the camera
+    [SerializeField] private Transform targetEnemy;  //reference to enemy
+    [SerializeField] private List<Transform> EnemyList;
+    [SerializeField] private float shootRange;  //range for shooting
+    [SerializeField] private float shootSpeed;  //attack speed
 
-    private Transform enemy;  //reference to enemy
     private Animator animator; //reference to animator
     private NavMeshAgent navAgent;  //reference to navmeshagent
 
-    private bool enemyClicked;
-    private bool isWalking;
+    private float rechargeTime = 2f;  //time between attacks
+    private bool enemyClicked;  //clicked on enemy?
+    private bool isWalking;  //am i walking?
 
     // Use this for initialization
     void Start()
@@ -26,71 +28,103 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //using ray cast to detect mouse clicks and position of clicks/mouse
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
 
-        //right click
-        if (Input.GetButtonDown("Fire 2"))
-        {
-            //raycast from mouse position, (shoots ray, outputs what it hit, distance ray goes)
-            if (Physics.Raycast(ray, out hit, 100))
-            {
-                //checks if collided with enemy and then moves toward enemy
-                if (hit.collider.CompareTag("Enemy"))
-                {
-                    enemy = hit.transform;  //move toward enemy
-                    enemyClicked = true;  //clicked enemy
-                }
-                else  //otherwise move character
-                {
-                    isWalking = true;  //walking
-                    enemyClicked = false;  //didnt cick enemy
-                    navAgent.destination = hit.point;  //sets destination
-                    navAgent.isStopped = false;  //checks if player has stopped
-                }
-            }
+        PlayerControls();
 
-            if(navAgent.remainingDistance <= navAgent.stoppingDistance)
-            {
-                isWalking = false;
-            }
-            else
-            {
-                isWalking = true;
-            }
-
-            animator.SetBool("IsWalking", isWalking);
-        }
-
+        //checks if click was on an enemy
         if (enemyClicked)
         {
             MoveAndShoot();
         }
     }
 
-    private void MoveAndShoot()
+    //movment and detect enemy
+    private void PlayerControls()
     {
-        if (enemy == null)
+        //using ray cast to detect mouse clicks and position of clicks/mouse
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        //right click
+        if (Input.GetButtonDown("Fire2"))
         {
-            return;
+            //raycast from mouse position, (shoots ray, outputs what it hit, distance ray goes)
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                //contains list of enemies in game and checks if clicked
+                foreach(Transform enemy in EnemyList)
+                {
+                    if(hit.collider.transform == enemy)
+                    {
+                        targetEnemy = hit.transform;  //make clicked enemy the target enemy
+                        enemyClicked = true;  //clicked enemy
+                    }
+                    else  //otherwise move character
+                    {
+                        targetEnemy = null;  //remove target
+                        isWalking = true;  //walking
+                        enemyClicked = false;  //didnt click enemy
+                        navAgent.destination = hit.point;  //sets destination
+                        navAgent.isStopped = false;  //checks if player has stopped
+                    }
+                }
+
+                ////checks player has a targetEnemy
+                //if (targetEnemy != null)
+                //{
+                //    enemyClicked = true;  //clicked enemy
+                //}
+
+            }
         }
-        navAgent.destination = enemy.position;
-        if (navAgent.remainingDistance >= shootRange)
+        //checks if destination was reached or in range of an enemy, then stops walking
+        if (navAgent.remainingDistance <= navAgent.stoppingDistance  || (navAgent.remainingDistance <= shootRange && targetEnemy != null))
         {
-            navAgent.isStopped = false;
+            isWalking = false;
+        }
+        else
+        {
             isWalking = true;
         }
 
-        if(navAgent.remainingDistance <=shootRange)
-        {
-            transform.LookAt(enemy);
+        animator.SetBool("isWalking", isWalking);  //calls walking animation
+    }
 
+
+    //moves player as well as shoots
+    private void MoveAndShoot()
+    {
+        //does enemy exist?
+        if (targetEnemy == null)
+        {
+            return;
+        }
+
+        //makes enemy target new desitination
+        navAgent.destination = targetEnemy.position;
+
+
+        //checks for range
+        if (navAgent.remainingDistance <= shootRange)
+        {
+            navAgent.isStopped = true;
+            isWalking = false;
+        }
+
+        //shoots if in range
+        if (navAgent.remainingDistance <=shootRange)
+        {
+            //face target enemy
+            transform.LookAt(targetEnemy);
+
+            //used for attacking/shooting
             if(Time.time > shootSpeed)
             {
                 shootSpeed = Time.time + rechargeTime;
                 Fire();
             }
+            
+            //stops movement and animation
             navAgent.isStopped = true;
             isWalking = false;
         }
@@ -98,6 +132,6 @@ public class Player : MonoBehaviour
 
     private void Fire()
     {
-        //implement shooting
+        animator.SetTrigger("CastSpell");
     }
 }
